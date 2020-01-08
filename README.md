5f863a85588770d08824b8afa80ca2f7cd280a3f
ethanzhou <ethan_enhe@163.com>
2020-01-08 21:07:05 +0800

    zzeh done

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Reversi
 基于minmax搜索实现的黑白棋AI
+
+## 目录结构
+```
+Reversi/
+  |
+  +---_includes/
+  |       |
+  |       +---board.h
+  |       |
+  |       +---board_h_demo.cpp
+  |       |
+  |       +---terrain_value.txt
+  |
+  |
+  +---资料.md
+```
--- _includes/board.h
+++ _includes/board.h
@@ -31,6 +31,18 @@
 #define SIZE 8
 #define NR 10
 const int nxgo[8][2]={{-1,1},{-1,0},{-1,-1},{0,1},{0,-1},{1,1},{1,0},{1,-1}};
+const int terrain[10][10]={
+	{  0,   0,   0,  0,  0,  0,  0,   0,   0},
+	{  0, 100, -50, 20, 10, 10, 20, -50, 100},
+	{  0, -50, -75, 10,  5,  5, 10, -75, -50},
+	{  0,  20,  10,  5,  2,  2,  5,  10,  20},
+	{  0,  10,   5,  2,  0,  0,  2,   5,  10},
+	{  0,  10,   5,  2,  0,  0,  2,   5,  10},
+	{  0,  20,  10,  5,  2,  2,  5,  10,  20},
+	{  0, -50, -75, 10,  5,  5, 10, -75, -50},
+	{  0, 100, -50, 20, 10, 10, 20, -50, 100},
+	{  0,   0,   0,  0,  0,  0,  0,   0,   0}
+};
 
 struct Reversi_Board{
 	/*
@@ -38,19 +50,23 @@ struct Reversi_Board{
 	 * 后手 -1 白棋
 	 * 空地  0
 	 */
-	int board[NR][NR],step=0;
+	int board[NR][NR],step;
 	inline bool inboard(int x,int y){return (x>0 && x<=SIZE && y>0 && y<=SIZE);}
 
 	//init & print
 	Reversi_Board(){
+		step=0;
 		memset(board,0,sizeof(board));
 		board[4][4]=board[5][5]=-1;
 		board[4][5]=board[5][4]=1;
 	}
 	Reversi_Board(int arr[NR][NR]){
+		step=-4;
 		for(int i=1;i<=SIZE;i++)
-			for(int j=1;j<=SIZE;j++)
+			for(int j=1;j<=SIZE;j++){
 				board[i][j]=arr[i][j];
+				if(arr[i][j])step++;
+			}
 	}
 	void prt(){
 		puts("+----------------+");
@@ -69,14 +85,39 @@ struct Reversi_Board{
 
 	//Calculation
 
-
 	/*
+	 * 判断是否有输赢
+	 *  1是黑赢
+	 * -1是白赢
+	 *  0是平局
+	 * -2是尚未分出胜负
+	 */
+	int win(){
+		int black=0,white=0;
+		for(int i=1;i<=SIZE;i++)
+			for(int j=1;j<=SIZE;j++){
+				if(board[i][j]==1)black++;
+				else if(board[i][j]==-1)white++;
+			}
+		if(step==60){
+			if(black>white)return 1;
+			else if(white>black)return -1;
+			else return 0;
+		}
+		else{
+			if(!white)return 1;
+			else if(!black)return -1;
+			else return -2;
+		}
+	}
+	/*
+	 * (可以用这个函数计算合理的走棋位置)
 	 * 计算吃子的函数
 	 * flip 表示是否翻转棋子
 	 * cur 表示当前是哪方
 	 * 返回值表示能反转多少个
 	 */
-	int calculate(bool flip,int x,int y,int cur){
+	int eat(bool flip,int x,int y,int cur){
 		int cnt=0;
 		for(int pos=0;pos<8;pos++){
 			int tx=x+nxgo[pos][0],ty=y+nxgo[pos][1],tcnt=0;
@@ -93,8 +134,68 @@ struct Reversi_Board{
 				}
 			}
 		}
+		cnt+=flip;
 		return cnt;
-	}	
+	}
+	int terr(int cur){
+		int cnt=0;
+		for(int i=1;i<=SIZE;i++)
+			for(int j=1;j<=SIZE;j++)
+				if(board[i][j]==cur)cnt+=terrain[i][j];
+		return cnt;
+	}
+	//返回cur可以下棋的地方个数
+	int choice(int cur){
+		int cnt=0;
+		for(int i=1;i<=SIZE;i++)
+			for(int j=1;j<=SIZE;j++)
+				if(eat(0,i,j,cur))cnt++;
+		return cnt;
+	}
+	//计算稳定子
+	int not_threated(int cur){
+		cur=-cur;
+		bool threat[NR][NR];
+		memset(threat,0,sizeof(threat));
+		for(int i=1;i<=SIZE;i++)
+			for(int j=1;j<=SIZE;j++){
+				//如果不是空白不行
+				if(board[i][j])continue;
+				for(int pos=0;pos<8;pos++){
+					int tx=i+nxgo[pos][0],ty=j+nxgo[pos][1];
+					//检验
+					while(inboard(tx,ty) && board[tx][ty]==-cur)
+						tx=tx+nxgo[pos][0],ty=ty+nxgo[pos][1];
+					//符合翻的要求
+					if(inboard(tx,ty) && board[tx][ty]==cur){
+						tx=i+nxgo[pos][0],ty=j+nxgo[pos][1];
+						while(inboard(tx,ty) && board[tx][ty]==-cur)
+							threat[tx][ty]=1,tx=tx+nxgo[pos][0],ty=ty+nxgo[pos][1];
+					}
+				}
+			}
+		int cnt=0;
+		for(int i=1;i<=SIZE;i++)
+			for(int j=1;j<=SIZE;j++)
+				cnt+=(!threat[i][j] && board[i][j]==-cur);
+		return cnt;
+	}
+
+
+	/*
+	 * 对于cur，棋局状态如何
+	 */
+	double assess(int cur){
+		int iswin=win();
+		if(iswin!=-2){
+			if(iswin==cur)return 1000000;
+			else if(iswin==-cur)return -1000000;
+			else return 0;
+		}
+		return 10*(terr(cur)-terr(-cur))+
+			100*(choice(cur)-choice(-cur))+
+			50*(not_threated(cur)-not_threated(-cur));
+	}
 
 
 	//API
@@ -108,7 +209,7 @@ struct Reversi_Board{
 	bool putchess(int x,int y,int cur){
 		if(!inboard(x,y) || board[x][y])return 0;
 		board[x][y]=cur;
-		calculate(1,x,y,cur);
+		eat(1,x,y,cur);
 		return 1;
 	}
 
