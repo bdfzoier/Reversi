daca3ad071a0564e66a2227bb9840b91303eb9cd
Guoyh06 <46080958+Guoyh06@users.noreply.github.com>
2020-01-09 12:40:51 +0800

    Update min-max.cpp

--- _includes/min-max.cpp
+++ _includes/min-max.cpp
@@ -1,4 +1,4 @@
-# include <board.h>
+# include "board.h"
 # include <cstdio>
 # include <cstring>
 # include <algorithm>
@@ -16,12 +16,12 @@ int arr[10][10]={
 	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 };
-pair<pair<int, int>, double> min_max(Reversi_Board nowBoard, int depth, bool isMax, int alpha, int beta){ //返回一个坐标 和 最大/最小权值
+pair<pair<int, int>, double> min_max(Reversi_Board nowBoard, int depth, bool isMax, double alpha, double beta){ //返回一个坐标 和 最大/最小权值
 	if (depth <= 0){
 		return make_pair(make_pair(-1, -1), nowBoard.assess(1));
 	}
 	int cur = isMax ? 1: -1;
-	double fnlWght;
+	double fnlWght = isMax ? -1e9 : 1e9;
 	pair <int, int> fnlChs;
 	for (int i = 1; i <= 8; i++){
 		for (int j = 1; j <= 8; j++){
