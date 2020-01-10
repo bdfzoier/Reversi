f8567929b546f4f5e5f3b1f58e8cdd1784d05660
FilaretKun <43267021+FilaretKun@users.noreply.github.com>
2020-01-10 23:38:43 +0800

    Made minor changes to reversi_ai.cpp
    added TONS of spaces
    added graphics settings
    added valid moves hint

--- reversi_ai.cpp
+++ reversi_ai.cpp
@@ -1,44 +1,68 @@
-#include<iostream>
-#include<cstdio>
-#include "./includes/board.h"
+# include <iostream>
+# include <cstdio>
+# include "./includes/board.h" 
+
 using namespace std;
-char first;
-int main(){
-	Reversi_Board myboard;
-	printf("you go first?[y/n]:");
-	scanf("%c",&first);
-	int cur=int(first=='y')*2-1;
+
+int cur;
+char first, hq;
+Reversi_Board myboard;
+
+void cl() {
 #ifdef linux
-		system("clear");
+	system("clear");
 #endif
+
 #ifndef linux
-		system("cls");
+	system("cls");
 #endif
-	while(myboard.win()==-2){
-		printf("Please enter the coordinate where you want to put your chess\n");
-		printf("e.g. D3,C6\n");
+}
+
+void pr() {
+	if(hq == 'n') {
 		myboard.raw_prt();
-		if(cur==1 && myboard.choice(first=='y'?1:-1)){
+	} else {
+		myboard.prt();
+	}
+}
+
+int main(){
+	printf("Do you want high quality graphics? (high quality graphics might not work on some computers) [y/n]:");
+	scanf("%c", &hq); getchar();
+	
+	printf("Are you going to go first? [y/n]:");
+	scanf("%c", &first);
+	cur=int(first=='y')*2-1;
+	
+	cl();
+
+	while (myboard.win() == -2) {
+		printf("Current player's valid moves are: ");
+		for(int j = 1 ; j <= 8 ; j ++) 
+			for(int i = 1 ; i <= 8 ; i ++) 
+				if(myboard.eat(0, i, j, cur) && myboard.board[i][j] == 0) 
+					printf("[%c%d] ", char(j + 'A' - 1), i);
+		printf("\n");
+		printf("Please enter the the new piece's coordinate\n");
+		printf("Format e.g.: E4, D6\n");
+		pr();
+		
+		if(cur == 1 && myboard.choice(first == 'y' ? 1 : -1)) {
 			char s[3];
 			scanf("%s",s);
-			int y=s[0]-'A'+1,x=s[1]-'0';
-			while(myboard.board[x][y] || !myboard.eat(0,x,y,first=='y'?1:-1)){
-				printf("Invalid!! Please enter again\n");
-				scanf("%s",s);
-				y=s[0]-'A'+1,x=s[1]-'0';
+			int x = s[1] - '0', y = s[0] - 'A' + 1;
+			while(myboard.board[x][y] || !myboard.eat(0, x, y, first == 'y' ? 1 : -1)) {
+				printf("Invalid coordinate! Please enter again\n");
+				scanf("%s", s);
+				x = s[1] - '0', y = s[0] - 'A' + 1;
 			}
-			myboard.putchess(x,y,first=='y'?1:-1);
+			myboard.putchess(x, y, first == 'y' ? 1 : -1);
 		}
 		else
-			myboard.auto_putchess(first=='y'?-1:1);
-#ifdef linux
-		system("clear");
-#endif
-#ifndef linux
-		system("cls");
-#endif
-		cur=-cur;
+			myboard.auto_putchess(first == 'y' ? -1 : 1);
+		cl();
+		cur = -cur;
 	}
-	printf("%d wins",myboard.win());
+	printf("%d wins", myboard.win());
 	return 0;
 }
